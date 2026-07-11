5f4753dc571705d61cb3009884fc21faedc659de
shivdhigan01-droid <shivdhigan01@gmail.com>
2026-07-11 13:39:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,23 @@ const openBtn = document.getElementById("openBtn");
 document.getElementById("bgMusic").play();
 const intro = document.getElementById("intro");
 const birthday = document.getElementById("birthday");
-
 openBtn.addEventListener("click", () => {
 
     intro.style.display = "none";
-    birthday.style.display = "block";
 
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+    const rose = document.getElementById("rose");
+
+    rose.style.display = "block";
+
+    setTimeout(() => {
+
+        rose.style.display = "none";
+        birthday.style.display = "block";
+
+    }, 4000);
+
+});
+
 
 });
 // Floating Hearts 💗
