9a35304b4797c2086624e050144b34aa78475e72
shivdhigan01-droid <shivdhigan01@gmail.com>
2026-07-11 13:10:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -76,3 +76,36 @@ s.style.textShadow = "0 0 12px gold";
 }
 
 setInterval(createSparkle,500);
+// 🌸 Falling Petals
+
+function createPetal() {
+
+    const petal = document.createElement("div");
+
+    petal.innerHTML = "🌸";
+    petal.style.position = "fixed";
+    petal.style.left = Math.random() * 100 + "vw";
+    petal.style.top = "-30px";
+    petal.style.fontSize = (18 + Math.random() * 15) + "px";
+    petal.style.pointerEvents = "none";
+    petal.style.zIndex = "9998";
+    petal.style.opacity = "0.9";
+
+    document.body.appendChild(petal);
+
+    let y = -30;
+    let x = Math.random() * 30 - 15;
+
+    const fall = setInterval(() => {
+        y += 2;
+        petal.style.top = y + "px";
+        petal.style.transform = `translateX(${x}px) rotate(${y}deg)`;
+
+        if (y > window.innerHeight) {
+            clearInterval(fall);
+            petal.remove();
+        }
+    }, 20);
+}
+
+setInterval(createPetal, 900);
